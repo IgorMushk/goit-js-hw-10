4af5ff7e649b9001c337616779de87b7d0fa9765
IgorMushk <igor.mushketik@gmail.com>
2023-07-27 10:53:04 +0300

    Del comments

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ const errorIndicator = document.querySelector('.error');
 const cardBreedCat = document.querySelector('.cat-info');
 const selectBreedCat = document.querySelector('.breed-select');
 
-//const BASE_URL = 'https://api.thecatapi.com/v1';
-//const API_KEY =
-//  'live_DWnryEvKWXcqeFK76djLNUR95YisUbO5IiYhoUFb6kOXJ7d52fSO10G4XSngGkna';
-
 let storedBreeds = [];
 
 selectBreed.classList.add('is-hidden');
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,19 +1,8 @@
-//const selectBreed = document.querySelector('select.breed-select');
-//const loaderIndicator = document.querySelector('.loader');
-//const errorIndicator = document.querySelector('.error');
-//const cardBreedCat = document.querySelector('.cat-info');
-
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_DWnryEvKWXcqeFK76djLNUR95YisUbO5IiYhoUFb6kOXJ7d52fSO10G4XSngGkna';
 
-//let storedBreeds = [];
-
-//selectBreed.classList.add('is-hidden');
-//errorIndicator.classList.add('is-hidden');
-
 export function fetchBreeds() {
-  //fetch(`${BASE_URL}/breeds`, {
   return fetch(`${BASE_URL}/breeds`, {
     headers: {
       'x-api-key': API_KEY,
@@ -24,28 +13,9 @@ export function fetchBreeds() {
     }
     return response.json();
   });
-  // .then(data => {
-  //   selectBreed.classList.remove('is-hidden');
-  //   loaderIndicator.classList.add('is-hidden');
-  //   storedBreeds = data;
-
-  //   let markup = null;
-  //   markup = storedBreeds
-  //     .map(breed => {
-  //       return `<option value="${breed.id}">${breed.name}</option>`;
-  //     })
-  //     .join('');
-  //   selectBreed.innerHTML = markup;
-  // })
-  // .catch(function (error) {
-  //   errorIndicator.classList.remove('is-hidden');
-  //   loaderIndicator.classList.add('is-hidden');
-  //   console.log(error);
-  // });
 }
 
 export function fetchCatByBreed(breedId) {
-  //fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`, {
   return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`, {
     headers: {
       'x-api-key': API_KEY,
@@ -56,25 +26,4 @@ export function fetchCatByBreed(breedId) {
     }
     return response.json();
   });
-  // .then(data => {
-  //   selectBreed.classList.remove('is-hidden');
-  //   loaderIndicator.classList.add('is-hidden');
-
-  //   console.log(data);
-
-  //   const markup = `
-  //         <img src="${data[0].url}" alt="${data[0].breeds[0].name}" width="480" />
-  //         <div class="cat-info-text">
-  //             <h2>${data[0].breeds[0].name}</h2>
-  //             <p>${data[0].breeds[0].description}</p>
-  //             <p><span>Temperament :</span>${data[0].breeds[0].temperament}</p>
-  //         </div>`;
-  //   cardBreedCat.innerHTML = markup;
-  //   console.log(markup);
-  // })
-  // .catch(function (error) {
-  //   errorIndicator.classList.remove('is-hidden');
-  //   loaderIndicator.classList.add('is-hidden');
-  //   console.log(error);
-  // });
 }
